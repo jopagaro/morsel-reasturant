c739d94e5adca1d8a27dc872d062ed616824f5c2
jopagaro <rocoroche13@gmail.com>
2025-11-28 17:08:23 -0500

    Initial Commit

---
+++ morsel-reasturant/morsel_reasturantApp.swift
@@ -0,0 +1,17 @@
+//
+//  morsel_reasturantApp.swift
+//  morsel-reasturant
+//
+//  Created by John Paul Roche on 11/28/25.
+//
+
+import SwiftUI
+
+@main
+struct morsel_reasturantApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ morsel-reasturantTests/morsel_reasturantTests.swift
@@ -0,0 +1,17 @@
+//
+//  morsel_reasturantTests.swift
+//  morsel-reasturantTests
+//
+//  Created by John Paul Roche on 11/28/25.
+//
+
+import Testing
+@testable import morsel_reasturant
+
+struct morsel_reasturantTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
